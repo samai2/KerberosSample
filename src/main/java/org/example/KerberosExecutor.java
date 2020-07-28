421f60fd68aa326dc5c4e8e6cc614d9b965517a2
Maxim Shadrin <Maxim.shadrin@al-enterprise.com>
2020-07-28 12:55:49 +0300

    Kerb5.conf is optional

--- src/main/java/org/example/KerberosExecutor.java
+++ src/main/java/org/example/KerberosExecutor.java
@@ -40,17 +40,20 @@ public class KerberosExecutor {
      */
     private static void propertiesSetup() throws FileNotFoundException {
 
-        String PATH_TO_CONFIG = "auth.config";
+        String PATH_TO_JAAS_CONFIG = "auth.config";
 
-        File file = new File(PATH_TO_CONFIG);
+        File file = new File(PATH_TO_JAAS_CONFIG);
         if (!file.exists()) {
             throw new FileNotFoundException("Please ensure auth.config in the same directory as the executable jar");
+        }
+        file = new File("krb5.conf");
+        if(file.exists()){
+            System.setProperty("java.security.krb5.conf", "krb5.conf");
         }
 
         System.setProperty("javax.security.auth.useSubjectCredsOnly", "true");
         System.setProperty("sun.security.krb5.debug", "true");
-        System.setProperty("java.security.auth.login.config", PATH_TO_CONFIG);
-        System.setProperty("java.security.krb5.conf", "krb5.conf");
+        System.setProperty("java.security.auth.login.config", PATH_TO_JAAS_CONFIG);
     }
 
     static Subject getAuthContext() throws Exception {
